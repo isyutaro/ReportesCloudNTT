c79f4b934b94ab223ee689b1e9c37319370376c4
root <root@c11032817.interalia.net>
2013-05-08 18:26:28 -0500

    cambio sql para graficas bits y bytes

--- SQL.py
+++ SQL.py
@@ -5,7 +5,8 @@ class SQL:
         def getListaCacti(self):
                 db = MySQLdb.connect(user=MySQL_USER, db=MySQL_DB, passwd=MySQL_PW, host='localhost')
                 cursor = db.cursor()
-                cursor.execute('select host.description, graph_local.id from host, graph_local, graph_templates where host.id=graph_local.host_id and graph_templates.id=graph_local.graph_template_id and graph_templates.id=33')
+                #cursor.execute('select host.description, graph_local.id from host, graph_local, graph_templates where host.id=graph_local.host_id and graph_templates.id=graph_local.graph_template_id and graph_templates.id=33')
+                cursor.execute('select host.description, graph_local.id from host, graph_local, graph_templates where host.id=graph_local.host_id and graph_templates.id=graph_local.graph_template_id and (graph_templates.id=32 or graph_templates.id=33) group by host.description')
                 lista = cursor.fetchall()
                 db.close()
                 return lista
